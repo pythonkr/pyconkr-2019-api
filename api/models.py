33cc6f136b94878b8b222d2025f87e6b1e057e42
amazingguni <amazingguni@gmail.com>
2018-12-10 23:19:36 +0900

    Add initial program models for testing

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from model_utils.managers import InheritanceManager
 
-# Create your models here.
+class Program(models.Model):
+    objects = InheritanceManager()
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+class Presentation(Program):
+    presentation_field = models.CharField(max_length=255, null=True, blank=True)
+
+class Tutorial(Program):
+    tutorial_field = models.CharField(max_length=255, null=True, blank=True)
+
+class Sprint(Program):
+    sprint_field = models.CharField(max_length=255, null=True, blank=True)
+
+class Youngcoder(Program):
+    youngcoder_field = models.CharField(max_length=255, null=True, blank=True)
+
+class Exercise(Program):
+    exercise_field = models.CharField(max_length=255, null=True, blank=True)
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from api.models import Program, Presentation, Tutorial, Sprint, Youngcoder, Exercise
 
-# Create your tests here.
+class ProgramTestCase(TestCase):
+    def test_can_retrieve_different_models_have_same_parent(self):
+        # Given
+        Presentation.objects.create(name='Presentation', 
+            presentation_field='Presentation Field')
+        Tutorial.objects.create(name='Tutorial', 
+            tutorial_field='Tutorial Field')
+        Sprint.objects.create(name='Sprint', 
+            sprint_field='Sprint Field')
+        Youngcoder.objects.create(name='Youngcoder', 
+            youngcoder_field='Youngcoder Field')
+        Exercise.objects.create(name='Exercise', 
+            exercise_field='Exercise Field')
+        
+        # When
+        programs = Program.objects.select_subclasses()
+
+        # Then
+        self.assertEquals(len(programs), 5)
+        self.assertTrue(any([type(program) is Presentation for program in programs]))
+        self.assertTrue(any([type(program) is Tutorial for program in programs]))
+        self.assertTrue(any([type(program) is Sprint for program in programs]))
+        self.assertTrue(any([type(program) is Youngcoder for program in programs]))
+        self.assertTrue(any([type(program) is Exercise for program in programs]))
+
