a572fa30a6a6b103e00eee5df7be19d4d6a203e5
amazingguni <amazingguni@gmail.com>
2018-12-14 21:34:26 +0900

    Apply pylint and fix warnings

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+# pylint: disable=too-few-public-methods
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- pyconkr/wsgi.py
+++ pyconkr/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
+# pylint: disable=invalid-name
 application = get_wsgi_application()
