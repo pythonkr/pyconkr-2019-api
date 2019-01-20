43b0c9119d48c8e342c11d9fd23cf52e7866d5c8
amazingguni <amazingguni@gmail.com>
2019-01-20 11:30:23 +0900

    Fix pylint error

--- pyconkr/urls.py
+++ pyconkr/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework import routers
 from graphene_django.views import GraphQLView
 from api import views
-from django.http import HttpResponse
 
 # pylint: disable=invalid-name
 router = routers.DefaultRouter()
