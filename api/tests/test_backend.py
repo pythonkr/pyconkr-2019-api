9349addf67b48739e6bd6a7aaaa5d3c83be1f7e8
amazingguni <amazingguni@gmail.com>
2019-02-16 18:28:01 +0900

    Add testcases

--- api/tests/test_backend.py
+++ api/tests/test_backend.py
@@ -13,17 +13,28 @@ USER_RESPONSE = {
     "bio": "Be a good engineer",
 }
 
+EMAILS_RESPONSE = [
+    {
+        "email": "amazingguni@pycon.kr",
+        "primary": True,
+        "verified": True,
+        "visibility": "public"
+    }
+]
+
 
 class OAuthTokenBackendTestCase(testcases.TestCase):
     def setUp(self):
         self.request_factory = RequestFactory()
         self.backend = OAuthTokenBackend()
+        self.dummy_user_response = USER_RESPONSE.copy()
+        self.dummy_emails_response = EMAILS_RESPONSE.copy()
 
     @mock.patch('api.oauth_tokenbackend.requests.get')
     def test_authenticate(self, mock_get):
         # Given
         mock_resp = self._mock_response(
-            status=200, json=USER_RESPONSE)
+            status=200, json=self.dummy_user_response)
 
         mock_get.side_effect = [mock_resp]
         request = self.request_factory.get('/')
@@ -36,6 +47,28 @@ class OAuthTokenBackendTestCase(testcases.TestCase):
         self.assertIsNotNone(user)
         self.assertEqual(f'github_{USER_RESPONSE["login"]}', user.username)
         self.assertEqual(USER_RESPONSE['email'], user.email)
+        self.assertEqual(USER_RESPONSE['login'], user.profile.name)
+        self.assertEqual(USER_RESPONSE['avatar_url'], user.profile.avatar_url)
+
+    @mock.patch('api.oauth_tokenbackend.requests.get')
+    def test_authenticate_without_public_email(self, mock_get):
+        # Given
+        self.dummy_user_response['email'] = None
+        mock_user_resp = self._mock_response(
+            status=200, json=self.dummy_user_response)
+        mock_email_resp = self._mock_response(
+            status=200, json=self.dummy_emails_response)
+        mock_get.side_effect = [mock_user_resp, mock_email_resp]
+        request = self.request_factory.get('/')
+
+        # When
+        user = self.backend.authenticate(
+            request=request, oauth_type='github', oauth_access_token='TEST_TOKEN')
+
+        # Then
+        self.assertIsNotNone(user)
+        self.assertEqual(f'github_{USER_RESPONSE["login"]}', user.username)
+        self.assertEqual(EMAILS_RESPONSE[0]['email'], user.email)
         self.assertEqual(USER_RESPONSE['login'], user.profile.name)
         self.assertEqual(USER_RESPONSE['avatar_url'], user.profile.avatar_url)
 
