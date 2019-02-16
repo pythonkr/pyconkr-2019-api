25338fff02d2f886ae1c8fecfa1d5e9f89375283
amazingguni <amazingguni@gmail.com>
2019-02-16 18:34:49 +0900

    Fix pylint error and remove unused methods

--- pyconkr/urls.py
+++ pyconkr/urls.py
@@ -1,30 +1,9 @@
 from django.views.decorators.csrf import csrf_exempt
-"""api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.http import HttpResponse
-from rest_framework import routers
 from graphene_django.views import GraphQLView
 from api import views
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
-# pylint: disable=invalid-name
-router = routers.DefaultRouter()
 
 
 urlpatterns = [
@@ -32,7 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', views.robots, name='robots.txt'),
     path('ping', lambda r: HttpResponse('OK')),
-    path('api/', include(router.urls), name='api'),
-    path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)), name='graphql'),
 ]
