0f5944c4cd8c008cb1644f6fd9e31a216cc533aa
amazingguni <amazingguni@gmail.com>
2019-02-16 16:37:53 +0900

    Fix typo error

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import get_user_model
-import graphql_jwt
-# from graphql_jwt.testcases import JSONWebTokenTestCase
+from graphql_jwt.testcases import JSONWebTokenTestCase
 
 
-class UsersTests(graphql_jwt.testcases.JSONWebTokenTestCase):
+class UsersTests(JSONWebTokenTestCase):
 
     def setUp(self):
         self.user = get_user_model().objects.create(username='test')
