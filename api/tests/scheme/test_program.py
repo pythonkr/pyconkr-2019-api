8e3b1a4a2e2c875e074e0f240677e97800e5f610
Hyunho Lee <mizzking75@gmail.com>
2019-02-17 21:14:39 +0900

    testcase of sponsor
    testcase

--- api/tests/scheme/test_program.py
+++ api/tests/scheme/test_program.py
@@ -132,3 +132,48 @@ class ProgramTestCase(BaseTestCase):
         result = schema.execute(query)
         actual = loads(dumps(result.data))
         self.assertDictEqual(actual, expected)
+
+    def test_retrieve_sponsor(self):
+        query = '''
+        query {
+            conference {
+                name_ko
+                desc_ko
+                name_en
+                desc_en
+                image
+                url
+                level {
+                    name
+                    desc
+                    price
+                    ticket_count
+                }
+                paid_at
+                ticket_users
+            }
+        }
+        '''
+
+        expected = {
+            'name_ko' : '파이콘준비위원회',
+            'desc_ko' : '파이콘을 준비하는 준비위원회입니다.',
+            'name_en' : 'PyconKr',
+            'desc_en' : 'The people who want to open python conference.',
+            'image'   : 'img',
+            'url'     : 'http://pythonkr/1',
+            'level'   : {
+                        'name' : '키스톤',
+                        'desc' : '가장돈은 많이 낸 분들이죠',
+                        'price': '20000000',
+                        'ticket_count' : '20'
+                        },
+            'paid_at' : 'datetime(2019, 8, 21, 13, 00).astimezone(tz=timezone)',
+            'ticket_users' : 'user'
+
+        }
+
+
+        result = schema.execute(query)
+        actual = loads(dumps(result.data))
+        self.assertDictEqual(actual, expected)
