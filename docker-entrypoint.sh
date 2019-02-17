c9d81990dfde1a8d854137a5770d4fad97440bdd
amazingguni <amazingguni@gmail.com>
2019-02-17 21:17:34 +0900

    Fix syntax error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,28 @@
 
 echo "Collect static files"
 python manage.py collectstatic --noinput
+
 echo "Apply database migrations"
 python manage.py migrate
+
 echo "Create admin user"
-USERNAME = 'pyconkr'
-EMAIL = 'pyconkr@pycon.kr'
-PASSWORD = ${PYCONKR_ADMIN_PASSWORD}
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('${USERNAME}', '${EMAIL}', '${PASSWORD}')"  | python manage.py shell | true
+CREATE_ADMIN_SOURCE="
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+# Create admin user
+USERNAME='pyconkr'
+EMAIL='pyconkr@pycon.kr'
+PASSWORD='${PYCONKR_ADMIN_PASSWORD}'
+
+try:
+    user = UserModel.objects.get(username=USERNAME)
+except UserModel.DoesNotExist:
+    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+"
+echo "${CREATE_ADMIN_SOURCE}"  | python manage.py shell
 
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
