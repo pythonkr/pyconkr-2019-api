547b068173b27bdfbc783d189675261af10465d0
amazingguni <amazingguni@gmail.com>
2019-02-17 23:31:49 +0900

    Revert

--- pyconkr/production_settings.py
+++ pyconkr/production_settings.py
@@ -8,7 +8,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'pyconkr',
+        'USER': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
