9fc65a15d84e5ed40a1d918e250616986579b628
amazingguni <amazingguni@gmail.com>
2019-02-21 22:58:53 +0900

    Set schedule nullable

--- api/models/program.py
+++ api/models/program.py
@@ -4,20 +4,20 @@ from model_utils.managers import InheritanceManager
 
 
 class Conference(models.Model):
-    name = models.CharField(max_length=50, default='', blank=True)
-    conference_started_at = models.DateField(null=True)
-    conference_finished_at = models.DateField(null=True)
-    sprint_started_at = models.DateField(null=True)
-    sprint_finished_at = models.DateField(null=True)
-    tutorial_started_at = models.DateField(null=True)
-    tutorial_finished_at = models.DateField(null=True)
+    name = models.CharField(max_length=50)
+    conference_started_at = models.DateField(null=True, blank=True)
+    conference_finished_at = models.DateField(null=True, blank=True)
+    sprint_started_at = models.DateField(null=True, blank=True)
+    sprint_finished_at = models.DateField(null=True, blank=True)
+    tutorial_started_at = models.DateField(null=True, blank=True)
+    tutorial_finished_at = models.DateField(null=True, blank=True)
 
-    presentation_proposal_started_at = models.DateField(null=True)
-    presentation_proposal_finished_at = models.DateField(null=True)
-    sprint_proposal_started_at = models.DateField(null=True)
-    sprint_proposal_finished_at = models.DateField(null=True)
-    tutorial_proposal_started_at = models.DateField(null=True)
-    tutorial_proposal_finished_at = models.DateField(null=True)
+    presentation_proposal_started_at = models.DateField(null=True, blank=True)
+    presentation_proposal_finished_at = models.DateField(null=True, blank=True)
+    sprint_proposal_started_at = models.DateField(null=True, blank=True)
+    sprint_proposal_finished_at = models.DateField(null=True, blank=True)
+    tutorial_proposal_started_at = models.DateField(null=True, blank=True)
+    tutorial_proposal_finished_at = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
