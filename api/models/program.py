de9ebb2076cc3b548962df00130534cbbf1483c7
amazingguni <amazingguni@gmail.com>
2019-03-13 23:03:01 +0900

    Modify name restriction

--- api/models/program.py
+++ api/models/program.py
@@ -55,8 +55,7 @@ class Program(models.Model):
 
     objects = InheritanceManager()
 
-    name = models.CharField(max_length=255, blank=True, default='',
-                            validators=[MinLengthValidator(1)])
+    name = models.CharField(max_length=255, null=True, validators=[MinLengthValidator(1)])
     desc = models.TextField(blank=True, default='')
     price = models.IntegerField(default=0)
     visible = models.BooleanField(default=False)
