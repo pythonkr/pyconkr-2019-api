a9c3540560f8c7a50ddabee17e545429c2722e3d
amazingguni <amazingguni@gmail.com>
2019-03-13 23:32:26 +0900

    Remove unused validator

--- api/models/program.py
+++ api/models/program.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
 from model_utils.managers import InheritanceManager
 
 
