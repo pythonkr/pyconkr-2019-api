629149fe3d1d15467defb415822e8c93a7f9ba05
amazingguni <amazingguni@gmail.com>
2019-03-31 10:58:01 +0900

    스폰서 관련 필드를 정의하였습니다.

--- api/models/sponsor.py
+++ api/models/sponsor.py
@@ -15,16 +15,34 @@ class SponsorLevel(models.Model):
 
 
 class Sponsor(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sponsor_owner')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sponsor_owner')
     name = models.CharField(max_length=255, null=True, blank=True)
-    desc = models.TextField(null=True, blank=True)
-    image = SorlImageField(upload_to='sponsor', null=True, blank=True)
-    url = models.CharField(max_length=255, null=True, blank=True)
+    manager_name = models.CharField(max_length=100, blank=True, default='')
+    manager_phone = models.CharField(max_length=100, blank=True, default='')
+    manager_secondary_phone = models.CharField(max_length=100, blank=True, default='')
+    manager_email = models.EmailField(blank=True, default='')
     level = models.ForeignKey(SponsorLevel, null=True,
                               on_delete=models.SET_NULL, blank=True)
-    paid_at = models.DateTimeField(null=True, blank=True)
-    ticket_users = models.ForeignKey(User, on_delete=models.SET_NULL,
-                                     null=True, blank=True, related_name='sponsor_ticket_users')
+    business_registration_number = models.CharField(max_length=100, blank=True, default='')
+    business_registration_image = SorlImageField(upload_to='sponsor_business_registration', blank=True, default='')
+
+    contact_process_required = models.BooleanField(default=False)
+    url = models.CharField(max_length=255, null=True, blank=True)
+    logo_image = SorlImageField(upload_to='sponsor_logo', null=True, blank=True)
+    desc = models.TextField(null=True, blank=True)
+
+    paid_at = models.DateTimeField(null=True, blank=True,
+                                   help_text='후원금이 입금된 일시')
+    accepted = models.BooleanField(default=False)
+
+    # # one to many로 변경해야함
+    # ticket_users = models.ForeignKey(User, on_delete=models.SET_NULL,
+    #                                  null=True, blank=True, related_name='sponsor_ticket_users')
+    # # one to many로 변경해야함
+    # presentations = models.ForeignKey(Presentation, on_delete=models.SET_NULL,
+    #                                  null=True, blank=True, related_name='sponsor_presentations')
+
+
 
     def __str__(self):
         return f'{self.name}/{self.level}'
