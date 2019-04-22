05edf45caa0a11e42361f94dd833cdbd3410d7a6
Bae KwonHan <darjeeling@gmail.com>
2019-04-22 19:49:56 +0900

    add api.models.utils.notify_slack

--- api/models/utils.py
+++ api/models/utils.py
@@ -1,5 +1,7 @@
 import hashlib
 from functools import partial
+from slacker import Slacker
+from django.conf import settings
 
 
 def hash_file(file, block_size=65536):
@@ -8,3 +10,15 @@ def hash_file(file, block_size=65536):
         hasher.update(buf)
 
     return hasher.hexdigest()
+
+
+def notify_slack(channel, message):
+    if not hasattr(settings, "SLACK_TOKEN"):
+        return False
+    try:
+        slack = Slacker(settings.SLACK_TOKEN)
+        slack.chat.post_message(channel, message)
+        # TODO need to logging more
+    except:
+        return False
+    return True
