217c45db786fc3d43b074381b40e17fe7e24d608
amazingguni <amazingguni@gmail.com>
2019-06-02 21:20:47 +0900

    Try move to s3

--- pyconkr/staging_settings.py
+++ pyconkr/staging_settings.py
@@ -15,10 +15,24 @@ DATABASES = {
     }
 }
 
-MEDIA_ROOT = '/media'
-MEDIA_URL = 'https://dev.pycon.kr/api/media/'
-STATIC_URL = '/api/static/'
-STATIC_ROOT = '/static'
+# MEDIA_ROOT = '/media'
+# MEDIA_URL = 'https://dev.pycon.kr/api/media/'
+# STATIC_URL = '/api/static/'
+# STATIC_ROOT = '/static'
+
+aws_env_keys = ['DEV_AWS_ACCESS_KEY_ID', 'DEV_AWS_SECRET_ACCESS_KEY', 'DEV_AWS_STORAGE_BUCKET_NAME']
+
+for key in aws_env_keys:
+    if not os.getenv(key):
+        print(f'You should set {key} into ~/.profile')
+        exit(1)
+
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.getenv('DEV_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('DEV_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('DEV_AWS_STORAGE_BUCKET_NAME')
 
 LOGGING = {
     'version': 1,
