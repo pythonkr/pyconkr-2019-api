a7fade569689c46b0e2d1b217287b0bc2fdbf5a7
Kookheon Kwon <kucuny@gmail.com>
2019-06-05 20:33:41 +0900

    Optimize import

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import sentry_sdk
 from django.utils.translation import ugettext_lazy as _
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
