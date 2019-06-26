3b26ddb6efd010a3b7e047dd273cc0fc747033ae
amazingguni <amazingguni@gmail.com>
2019-06-26 21:55:55 +0900

    공지에 export import 기능 추가

--- notice/admin.py
+++ notice/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from notice.models import Notice
 
 
-class NoticeAdmin(admin.ModelAdmin):
+class NoticeResource(resources.ModelResource):
+    class Meta:
+        model = Notice
+
+
+class NoticeAdmin(ImportExportModelAdmin):
+    resource_class = NoticeResource
     list_display = ('id', 'title_ko', 'title_en', 'link', 'published_at')
 
 
