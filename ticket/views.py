257297cf4d505c861265f160eab14a01adccd7c4
김정환 <iz4blue@users.noreply.github.com>
2019-08-09 16:05:24 +0900

    튜토리얼 이름이 너무 길어서 줄여서 출력하는 것 까지 구현

--- ticket/views.py
+++ ticket/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, get_object_or_404
 from graphql_relay import from_global_id
 
-from ticket.models import Ticket
+from ticket.models import Ticket, TicketProduct
 
 
 def group_required(*group_names):
@@ -21,9 +21,69 @@ def issue(request, global_id):
     ticket = get_object_or_404(Ticket, id=pk)
     profile = ticket.owner.profile
 
+    additional_keys = {
+        'is_patron': '개인후원 버튼을 전달',
+        'is_open_reviewer': '오픈리뷰어 버튼을 전달',
+        'is_speaker': '발표자 버튼을 전달',
+        'is_tutorial_owner': '튜토리얼 진행자 버튼을 전달',
+        'is_sprint_owner': '스프린트 진행자 버튼을 전달',
+        'has_youngcoder': '영코더 버튼을 전달',
+        'has_childcare': '차일드 케어 내용을 안내',
+    }
+    additional_message = []
+
+    for key, value in additional_keys.items():
+        try:
+            addition = getattr(profile, key)
+            if addition:
+                additional_message.append(value)
+        except AttributeError:
+            pass
+
+    if 'tshirtsize' in ticket.options:
+        tshirtsize = ticket.options['tshirtsize']
+    else:
+        tshirtsize = 'XXXX'
+
+    tutorial_keys = {
+        154: '따릉이 데이터 크롤링',
+        155: '따릉이 데이터 분석',
+        156: 'Tensorflow 2.0',
+        157: 'TDD 방식의 웹 개발',
+        158: 'Streaming Data Pipeline',
+        159: 'Object Detection',
+        160: '자연어처리 논문 구현',
+        161: '얼굴 인식 알고리즘',
+        162: 'JupyterHub in K8s',
+        163: 'Django in Docker',
+        164: 'LibreOffice with python',
+        165: '파이썬 3대장',
+        166: '',
+        167: '',
+        168: '',
+        169: 'Pytest',
+        170: '',
+        171: '',
+        172: 'Django in Beanstalk',
+        173: '',
+        174: '',
+        175: 'Geopandas',
+        176: 'cliff',
+        177: '',
+        178: 'GluonNLP',
+    }
+
+    if ticket.product.type == TicketProduct.TYPE_TUTORIAL:
+        product = tutorial_keys[ticket.product.tutorial_set.first().id]
+    else:
+        product = None
+
     context = {
         'profile': profile,
-        'ticket': ticket
+        'ticket': ticket,
+        'product': product,
+        'additional_message': additional_message,
+        'tshirtsize': tshirtsize,
     }
 
     return render(request, 'issue.html', context=context)
