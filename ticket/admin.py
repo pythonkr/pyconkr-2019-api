755c591954ad0cf31e06d1d1883d6308fcc4c0e5
amazingguni <amazingguni@gmail.com>
2019-08-16 09:25:06 +0900

    등록 버그 수정

--- ticket/admin.py
+++ ticket/admin.py
@@ -172,7 +172,7 @@ class TicketForRegistrationAdmin(admin.ModelAdmin):
 
         # INFO: QR 코드로 읽는 경우를 가정한다.
         if search_term.startswith('https://www.pycon.kr/ticket/my-ticket?id='):
-            global_id = search_term.split('=')[1]
+            global_id = search_term.replace('https://www.pycon.kr/ticket/my-ticket?id=', '')
             _, pk = from_global_id(global_id)
             queryset = Ticket.objects.filter(id=pk)
             return queryset, False
